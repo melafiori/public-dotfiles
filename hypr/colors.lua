02a5ec82d9505334a181d92256af0848b71046e7
melafiori <cachilu@proton.me>
2026-07-11 04:37:47 -0400

    Update color configurations across multiple files and change wallpaper path

--- hypr/colors.lua
+++ hypr/colors.lua
@@ -1,5 +1,5 @@
 return {
-    image = "/home/melafiori/Pictures/walls/wallhaven-rqjgym.png",
+    image = "/home/melafiori/Pictures/walls/wallhaven-og2re5.jpg",
 
     background = "0xff121318",
 
@@ -9,7 +9,7 @@ return {
 
     inverse_on_surface = "0xff2f3036",
 
-    inverse_primary = "0xff4c5c92",
+    inverse_primary = "0xff4e5b92",
 
     inverse_surface = "0xffe3e1e9",
 
@@ -19,51 +19,51 @@ return {
 
     on_error_container = "0xffffdad6",
 
-    on_primary = "0xff1d2d61",
+    on_primary = "0xff1f2d61",
 
-    on_primary_container = "0xffdce1ff",
+    on_primary_container = "0xffdde1ff",
 
-    on_primary_fixed = "0xff03174b",
+    on_primary_fixed = "0xff06164b",
 
-    on_primary_fixed_variant = "0xff344479",
+    on_primary_fixed_variant = "0xff364379",
 
-    on_secondary = "0xff2b3042",
+    on_secondary = "0xff2c2f42",
 
     on_secondary_container = "0xffdee1f9",
 
-    on_secondary_fixed = "0xff161b2c",
+    on_secondary_fixed = "0xff171b2c",
 
-    on_secondary_fixed_variant = "0xff414659",
+    on_secondary_fixed_variant = "0xff424659",
 
     on_surface = "0xffe3e1e9",
 
-    on_surface_variant = "0xffc6c6d0",
+    on_surface_variant = "0xffc6c5d0",
 
-    on_tertiary = "0xff432740",
+    on_tertiary = "0xff43273f",
 
-    on_tertiary_container = "0xffffd7f6",
+    on_tertiary_container = "0xffffd7f4",
 
-    on_tertiary_fixed = "0xff2c122a",
+    on_tertiary_fixed = "0xff2c1229",
 
-    on_tertiary_fixed_variant = "0xff5b3d57",
+    on_tertiary_fixed_variant = "0xff5c3d56",
 
-    outline = "0xff8f909a",
+    outline = "0xff90909a",
 
     outline_variant = "0xff45464f",
 
-    primary = "0xffb6c4ff",
+    primary = "0xffb8c4ff",
 
-    primary_container = "0xff344479",
+    primary_container = "0xff364379",
 
-    primary_fixed = "0xffdce1ff",
+    primary_fixed = "0xffdde1ff",
 
-    primary_fixed_dim = "0xffb6c4ff",
+    primary_fixed_dim = "0xffb8c4ff",
 
     scrim = "0xff000000",
 
     secondary = "0xffc2c5dd",
 
-    secondary_container = "0xff414659",
+    secondary_container = "0xff424659",
 
     secondary_fixed = "0xffdee1f9",
 
@@ -71,13 +71,13 @@ return {
 
     shadow = "0xff000000",
 
-    source_color = "0xff9899a5",
+    source_color = "0xff132154",
 
     surface = "0xff121318",
 
     surface_bright = "0xff38393f",
 
-    surface_container = "0xff1e1f25",
+    surface_container = "0xff1f1f25",
 
     surface_container_high = "0xff292a2f",
 
@@ -89,16 +89,16 @@ return {
 
     surface_dim = "0xff121318",
 
-    surface_tint = "0xffb6c4ff",
+    surface_tint = "0xffb8c4ff",
 
     surface_variant = "0xff45464f",
 
-    tertiary = "0xffe3bada",
+    tertiary = "0xffe4bad9",
 
-    tertiary_container = "0xff5b3d57",
+    tertiary_container = "0xff5c3d56",
 
-    tertiary_fixed = "0xffffd7f6",
+    tertiary_fixed = "0xffffd7f4",
 
-    tertiary_fixed_dim = "0xffe3bada",
+    tertiary_fixed_dim = "0xffe4bad9",
 
 }
